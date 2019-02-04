56924984ac2a0b9acc13519224809c9dbb4613dd
Krzysztof Polak <kriss145@gmail.com>
2019-02-04 10:40:58 +0100

    Add extends to HTML and add base file to Twig

--- gulpfile.babel.js/tasks/html.js
+++ gulpfile.babel.js/tasks/html.js
@@ -29,12 +29,173 @@ const getDataOne = function (file) {
   return JSON.parse(fs.readFileSync(dataPath, 'utf8'))
 }
 
+var twigExtends = function (Twig) {
+  /**
+   * Tag "autoescape"
+   * @output - content between tags
+   */
+  Twig.exports.extendTag({
+    // unique name for tag type
+    type: 'autoescape',
+    // regex match for tag (autoescape white-space anything)
+    regex: /^autoescape\s+(.+)$/,
+    // this is a standalone tag and doesn't require a following tag
+    next: ['endautoescape'],
+    open: true,
+
+    // Runs when the template is rendered
+    parse: function (token, context, chain) {
+      // parse the tokens into a value with the render context
+      var output = Twig.parse.apply(this, [token.output, context])
+
+      return {
+        chain: false,
+        output: output
+      }
+    }
+  })
+
+  // a matching end tag type
+  Twig.exports.extendTag({
+    type: 'endautoescape',
+    regex: /^endautoescape$/,
+    next: [ ],
+    open: false
+  })
+
+  /**
+   * Tag "load"
+   * @output - empty
+   */
+  Twig.exports.extendTag({
+    // unique name for tag type
+    type: 'load',
+    // regex match for tag (load white-space anything)
+    regex: /^load\s+(.+)$/,
+    // this is a standalone tag and doesn't require a following tag
+    next: [ ],
+    open: true,
+
+    // runs on matched tokens when the template is loaded. (once per template)
+    compile: function (token) {
+      var expression = token.match[1]
+
+      // Compile the expression. (turns the string into tokens)
+      token.stack = Twig.expression.compile.apply(this, [{
+        type: Twig.expression.type.expression,
+        value: expression
+      }]).stack
+
+      delete token.match
+      return token
+    },
+
+    // Runs when the template is rendered
+    parse: function (token, context, chain) {
+      var output = ''
+
+      return {
+        chain: false,
+        output: output
+      }
+    }
+  })
+
+  /**
+   * Tag "static"
+   * @output - path to file
+   */
+  Twig.exports.extendTag({
+    // unique name for tag type
+    type: 'static',
+    // regex match for tag (load white-space anything)
+    regex: /^static\s+(.+)$/,
+    // this is a standalone tag and doesn't require a following tag
+    next: [ ],
+    open: true,
+
+    // runs on matched tokens when the template is loaded. (once per template)
+    compile: function (token) {
+      var expression = token.match[1]
+
+      // Compile the expression. (turns the string into tokens)
+      token.stack = Twig.expression.compile.apply(this, [{
+        type: Twig.expression.type.expression,
+        value: expression
+      }]).stack
+
+      delete token.match
+      return token
+    },
+
+    // Runs when the template is rendered
+    parse: function (token, context, chain) {
+      var output = context.static + token.stack[0].value
+
+      return {
+        chain: false,
+        output: output
+      }
+    }
+  })
+
+  /**
+   * Tag "url"
+   * @output - empty
+   */
+  Twig.exports.extendTag({
+    // unique name for tag type
+    type: 'url',
+    // regex match for tag (load white-space anything)
+    regex: /^url\s+(.+)$/,
+    // this is a standalone tag and doesn't require a following tag
+    next: [ ],
+    open: true,
+
+    // Runs when the template is rendered
+    parse: function (token, context, chain) {
+      var output = ''
+
+      return {
+        chain: false,
+        output: output
+      }
+    }
+  })
+
+  /**
+   * Tag "csrf_token"
+   * @output - empty
+   */
+  Twig.exports.extendTag({
+    // unique name for tag type
+    type: 'csrf_token',
+    // regex match for tag (csrf_token white-space anything)
+    regex: /^csrf_token$/,
+    // this is a standalone tag and doesn't require a following tag
+    next: [ ],
+    open: true,
+
+    // Runs when the template is rendered
+    parse: function (token, context, chain) {
+      var output = ''
+
+      return {
+        chain: false,
+        output: output
+      }
+    }
+  })
+};
+
 const htmlTask = () => {
   return gulp.src(paths.src)
     .pipe(data(getDataOne))
     .pipe(data(getData))
     .on('error', handleErrors)
-    .pipe(twig())
+    .pipe(twig({
+      extend: twigExtends
+    }))
     .on('error', handleErrors)
     .pipe(gulp.dest(path.join(global.production ? config.root.dist : '', paths.dest)))
     .pipe(gulpif(!global.production, browserSync.stream()))
