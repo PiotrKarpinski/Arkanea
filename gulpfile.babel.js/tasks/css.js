c45c4f1578fda8eebb178c99ddf1ea09c753f769
Krzysztof Polak <kriss145@gmail.com>
2019-04-03 14:04:38 +0200

    Add core, remove combine-mq package

--- gulpfile.babel.js/tasks/css.js
+++ gulpfile.babel.js/tasks/css.js
@@ -4,7 +4,6 @@ import posix from 'path-posix'
 import cssnano from 'gulp-cssnano'
 import sass from 'gulp-sass'
 import autoprefixer from 'gulp-autoprefixer'
-import combineMq from 'gulp-combine-mq'
 import gulpif from 'gulp-if'
 import handleErrors from '../lib/handle-errors'
 import config from '../config'
@@ -18,9 +17,6 @@ const cssTask = () => {
   return gulp.src(paths.src)
     .pipe(sass(config.tasks.css.sass))
     .on('error', handleErrors)
-    .pipe(combineMq({
-      beautify: false
-    }))
     .pipe(autoprefixer(config.tasks.css.autoprefixer))
     .pipe(gulpif(global.production, cssnano({ autoprefixer: false })))
     .pipe(gulp.dest(posix.join(global.production ? config.root.dist : '', paths.dest)))
